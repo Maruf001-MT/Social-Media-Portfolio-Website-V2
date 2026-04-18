57cb8020f766b0cb0ef4fb6cfc6a779d802e0e6d
Maruf Zisan <jisanmaruf562@gmail.com>
2026-04-19 02:06:47 +0600

    sdfsfd

---
+++ templates/navbar.js
@@ -0,0 +1,150 @@
+class HeaderTemplate extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+
+    <nav class="navbar">
+            <div class="padding-global is-small">
+                <div class="navbar_component">
+                    <div class="navbar_content">
+                        <div class="navbar_brand"><a href="/" aria-current="page" class="navbar_logo-wrap w-inline-block w--current"><img src="/assets/img/logo.svg" loading="lazy" alt="" class="navbar_logo"/></a>
+                            <div
+                                class="navbar_creative"><img src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/6840ad47034f7c5148f8b56b_bar-code-1.svg" loading="lazy" alt="" class="navbar_barcode" />
+                                <div class="navbar_creative-text">Maruf Zisan</div>
+                        </div>
+                    </div>
+                    <div class="navbar_links-menu">
+                        <div class="navbar_links">
+                            <a href="/" data-w-id="df70bfa9-fec6-06b6-1b9f-9402e5712ace" aria-current="page" class="navbar_link w-inline-block w--current">
+                                <div class="navbar_link-texts">
+                                    <div class="navbar_link-text _1">Home</div>
+                                    <div class="navbar_link-text _2">Home</div>
+                                </div>
+                                <div class="navbar_link-dot"></div>
+                            </a>
+
+                            <div class="navbar_works-link">
+                                <a href="/works/1" data-w-id="df70bfa9-fec6-06b6-1b9f-9402e5712ace" class="navbar_link w-inline-block">
+                                    <div class="navbar_link-texts">
+                                        <div class="navbar_link-text _1">Work</div>
+                                        <div class="navbar_link-text _2">Work</div>
+                                    </div>
+                                </a>
+                                <div class="navbar_works-number-wrap">
+                                    <div class="navbar_works-number">3</div>
+                                </div>
+                            </div>
+                            <a href="/blog/1" data-w-id="df70bfa9-fec6-06b6-1b9f-9402e5712ace" class="navbar_link w-inline-block">
+                                <div class="navbar_link-texts">
+                                    <div class="navbar_link-text _1">Blog</div>
+                                    <div class="navbar_link-text _2">Blog</div>
+                                </div>
+                                <div class="navbar_link-dot"></div>
+                            </a>
+                            <a href="/contact/1" data-w-id="df70bfa9-fec6-06b6-1b9f-9402e5712ace" class="navbar_link w-inline-block">
+                                <div class="navbar_link-texts">
+                                    <div class="navbar_link-text _1">Contact</div>
+                                    <div class="navbar_link-text _2">Contact</div>
+                                </div>
+                                <div class="navbar_link-dot"></div>
+                            </a>
+                        </div>
+                        <div class="menu_component">
+                            <div fs-scrolldisable-media="(max-width: 767px)" fs-scrolldisable-element="toggle" data-w-id="5fdd110c-b4dc-4c3b-f311-33bd343dda78" class="hamburger">
+                                <div class="hamburger_line _1"></div>
+                                <div class="hamburger_line _2"></div>
+                            </div>
+                            <div class="menu">
+                                <div class="menu_in">
+                                    <div class="menu_content">
+                                        <div class="menu_links">
+                                            <a href="/" data-w-id="34ea61be-4150-c0ab-66d0-326592b2e049" aria-current="page" class="menu_link w-inline-block w--current">
+                                                <div class="menu_link-texts">
+                                                    <div class="menu_link-text _1">Home</div>
+                                                    <div class="menu_link-text _2">Home</div>
+                                                </div>
+                                                <div class="menu_link-dot"></div>
+                                            </a>
+
+                                            <a href="/works/1" data-w-id="34ea61be-4150-c0ab-66d0-326592b2e049" class="menu_link w-inline-block">
+                                                <div class="menu_link-texts">
+                                                    <div class="menu_link-text _1">Work</div>
+                                                    <div class="menu_link-text _2">Work</div>
+                                                </div>
+                                                <div class="menu_link-dot"></div>
+                                            </a>
+                                            <a href="/blog/1" data-w-id="34ea61be-4150-c0ab-66d0-326592b2e049" class="menu_link w-inline-block">
+                                                <div class="menu_link-texts">
+                                                    <div class="menu_link-text _1">Blog</div>
+                                                    <div class="menu_link-text _2">Blog</div>
+                                                </div>
+                                                <div class="menu_link-dot"></div>
+                                            </a>
+                                            <a href="/contact/1" data-w-id="34ea61be-4150-c0ab-66d0-326592b2e049" class="menu_link w-inline-block">
+                                                <div class="menu_link-texts">
+                                                    <div class="menu_link-text _1">Contact</div>
+                                                    <div class="menu_link-text _2">Contact</div>
+                                                </div>
+                                                <div class="menu_link-dot"></div>
+                                            </a>
+                                        </div>
+                                        <div class="menu_actions">
+                                            <div class="menu_social-wrap">
+                                                <div class="menu_buttons">
+                                                    <a href="https://webflow.com/templates/designers/templatesupply" data-wf--button--variant="grey" data-w-id="059e00b0-f64e-1901-eae8-f1971aea4566" class="button-secondary w-inline-block">
+                                                        <div class="button_texts">
+                                                            <div class="button_text _1">Buy template</div>
+                                                            <div class="button_text _2">Buy template</div>
+                                                        </div>
+                                                        <div class="button-line_space blue-dark-36"></div>
+                                                        <div class="button-line_dot blue-dark-37">
+                                                            <div class="button_dot"></div>
+                                                            <div class="button_dot-scale"></div>
+                                                        </div>
+                                                    </a>
+                                                    <a href="https://www.template.supply/" data-wf--button--variant="grey" data-w-id="059e00b0-f64e-1901-eae8-f1971aea4566" target="_blank" class="button-secondary w-inline-block">
+                                                        <div class="button_texts">
+                                                            <div class="button_text _1">More templates</div>
+                                                            <div class="button_text _2">More templates</div>
+                                                        </div>
+                                                        <div class="button-line_space blue-dark-36"></div>
+                                                        <div class="button-line_dot blue-dark-37">
+                                                            <div class="button_dot"></div>
+                                                            <div class="button_dot-scale"></div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="menu_social"><a href="https://instagram.com" target="_blank" class="menu_social-link w-inline-block"><img loading="lazy" src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68642b559d94bcc3773fc317_social-insta.svg" alt="" class="menu_social-icon"/></a>
+                                                    <a
+                                                        href="https://x.com" target="_blank" class="menu_social-link w-inline-block"><img loading="lazy" src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68642b569d94bcc3773fc318_social-x.svg" alt="" class="menu_social-icon" /></a><a href="https://dribbble.com" target="_blank"
+                                                            class="menu_social-link w-inline-block"><img loading="lazy" src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/686464e5c7256ddd81c074ee_dribble.svg" alt="" class="menu_social-icon"/></a></div>
+                                            </div><a data-w-id="5fdd110c-b4dc-4c3b-f311-33bd343dda97" href="/contact/2" class="menu_contact w-inline-block"><img src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474f06add2ed267a218556_link.png" loading="lazy" sizes="(max-width: 1400px) 100vw, 1400px" srcset="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474f06add2ed267a218556_link-p-500.png 500w, https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474f06add2ed267a218556_link-p-800.png 800w, https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474f06add2ed267a218556_link-p-1080.png 1080w, https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474f06add2ed267a218556_link.png 1400w" alt="" class="menu_contact-img"/><div class="menu-contact-link"><div class="menu_contact-text">Contact</div><img src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/6847494a49644635f62a1bc0_arrow-white-diagonal.svg" loading="lazy" alt="" class="menu_contact-arrow"/></div></a>
+                                            <div
+                                                class="menu_legal">
+                                                <a href="/legal/privacy-policy" data-w-id="08715bc4-c4b7-73ba-45ba-b2d079cf51c2" class="menu_legal-link w-inline-block">
+                                                    <div class="menu_legal-text">Privacy Policy</div>
+                                                    <div class="menu_legal-line grey"></div>
+                                                </a>
+                                                <a href="/legal/terms-of-use" data-w-id="08715bc4-c4b7-73ba-45ba-b2d079cf51c2" class="menu_legal-link w-inline-block">
+                                                    <div class="menu_legal-text">Terms of use</div>
+                                                    <div class="menu_legal-line grey"></div>
+                                                </a>
+                                        </div>
+                                    </div>
+                                    <div data-w-id="5fdd110c-b4dc-4c3b-f311-33bd343ddaa2" class="menu_close"><img src="https://cdn.prod.website-files.com/6840876d4d1ed0e8e2a330b9/68474e628bed528dd7a4be9e_close.svg" loading="lazy" alt="" class="menu_close-icon" /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="menu_bg-blur"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar_line"></div>
+    </div>
+    </div>
+    </nav>
+
+        `;
+    }
+  }
+
+  customElements.define('navbar-template', HeaderTemplate);
